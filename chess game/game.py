edfeb77229ba86880457a8b4e75ee3f1daf40ac8
cohub77 <cohub77@gmail.com>
2023-05-05 05:42:48 -0600

    IM LOSNG MY MIND

--- chess game/game.py
+++ chess game/game.py
@@ -27,59 +27,46 @@ def move_piece(selected_piece, row, col, pieces):
             pieces.remove(piece)
             break
 
+def king_out_of_check_after_move(current_player, selected_piece, row, col, pieces):
+    pieces_copy = [piece.copy() if piece == selected_piece else piece for piece in pieces]
+    move_piece(selected_piece, row, col, pieces_copy)
+    return not is_in_check(current_player, pieces_copy)
+
 def on_click(event, selected_piece, current_player, pieces, board, draw_board, images):
     row = event.y // 50
     col = event.x // 50
     king_in_check = is_in_check(current_player, pieces)
 
     if not selected_piece:
-        # If no piece is selected, try to select one
-        for piece in pieces:
-            if piece.row == row and piece.col == col and piece.color == current_player:
-                selected_piece = piece
-                draw_board(board, pieces, images, selected_piece)
-                break
+        selected_piece = select_different_piece(row, col, current_player, pieces)
+        if selected_piece:
+            draw_board(board, pieces, images, selected_piece)
     else:
-        # If a piece is selected, handle move attempt
         if selected_piece.row == row and selected_piece.col == col:
-            # Deselect the piece if the clicked square is the current position of the piece
             selected_piece = None
             draw_board(board, pieces, images, selected_piece)
-        elif selected_piece.is_valid_move(row, col, pieces):
-            # If the move is valid, simulate it with a copy of the pieces list
-            pieces_copy = [piece.copy() if piece == selected_piece else piece for piece in pieces]
-            move_piece(selected_piece, row, col, pieces_copy)
+        else:
+            if selected_piece.is_valid_move(row, col, pieces):
+                king_out_of_check = king_out_of_check_after_move(current_player, selected_piece, row, col, pieces)
+                if king_in_check and king_out_of_check or not king_in_check:
+                    move_piece(selected_piece, row, col, pieces)
+                    draw_board(board, pieces, images, None)
+                    selected_piece = None
 
-            # Check if the move gets the player out of check
-            if not king_in_check or (king_in_check and not is_in_check(current_player, pieces_copy)):
-                move_piece(selected_piece, row, col, pieces)
+                    if current_player == "White":
+                        current_player = "Black"
+                    else:
+                        current_player = "White"
 
-                # Capture the opponent's piece if there is one in the destination square
-                for piece in pieces:
-                    if piece.row == row and piece.col == col and piece.color != selected_piece.color:
-                        pieces.remove(piece)
-                        break
-
-                draw_board(board, pieces, images, None)
-                selected_piece = None
-
-                # Switch to the other player
-                if current_player == "White":
-                    current_player = "Black"
+                    if is_in_check(current_player, pieces):
+                        print(f"{current_player} is in check!")
                 else:
-                    current_player = "White"
-
-                # Check if the next player is in check
-                if is_in_check(current_player, pieces):
-                    print(f"{current_player} is in check!")
+                    print(f"{current_player}'s king is in check. Invalid move.")
+                    selected_piece = None
+                    draw_board(board, pieces, images, selected_piece)
             else:
-                print(f"{current_player}'s king is in check. Invalid move.")
                 selected_piece = None
                 draw_board(board, pieces, images, selected_piece)
-        else:
-            # Deselect the piece after an invalid move
-            selected_piece = None
-            draw_board(board, pieces, images, selected_piece)
 
     return selected_piece, current_player
    
